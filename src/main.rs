bc1177541b7bd68423845495a0b4c7280cfd1562
Akshat Malviya <ancientfuture1337@gmail.com>
2026-08-31 00:49:34 +0900

    Update Config constructor and add error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem occured while parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for `{}`", config.query);
     println!("In the file `{}`", config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let file_text =
         fs::read_to_string(config.file_path).expect("Should've been able to read the file.");
 
@@ -20,12 +27,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // Temporary workaround for avoiding the borrow checker.
         // TODO: Optimize later.
+        if args.len() < 3 {
+            return Err("Not enough arguments provided!");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
